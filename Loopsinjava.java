267cc981ded559cbb34aa8d47d05054bdc096e07
saadi mohamed abderrahmen <abderrahmen.saadi9477@istic.ucar.tn>
2026-04-15 14:55:57 +0200

    loops in java

--- Loopsinjava.java
+++ Loopsinjava.java
@@ -1,10 +1,16 @@
 public class Loopsinjava {
     public static void main(String[] args) {
         int i = 0;
+        int j = 0;
         while (i < 5) {
             System.out.println("The value of i is: " + i);
             i++;
         }
+        do {
+            System.out.println("The value of j is: " + j);
+            j++;
+        } while (j < 5);
+
     }
     
 }
